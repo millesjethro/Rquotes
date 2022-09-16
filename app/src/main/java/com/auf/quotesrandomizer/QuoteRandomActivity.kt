bd6afcbacc95423392e2f066573c1fb199d41999
Milles Jethro D Del Rosario <milles.jethro16@gmail.com>
2022-09-16 19:57:39 +0800

    DISPLAY

--- app/src/main/java/com/auf/quotesrandomizer/QuoteRandomActivity.kt
+++ app/src/main/java/com/auf/quotesrandomizer/QuoteRandomActivity.kt
@@ -1,11 +1,33 @@
 package com.auf.quotesrandomizer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.auf.quotesrandomizer.databinding.ActivityGetRandomQuotesBinding
 
-class QuoteRandomActivity : AppCompatActivity() {
+
+class QuoteRandomActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityGetRandomQuotesBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityGetRandomQuotesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val intent = getIntent()
+        var whatQuote = intent.getExtras()?.getString("Quotess");
+        binding.textView3.text = whatQuote
+
+    }
+
+    override fun onClick(p0: View?) {
+        val intent = getIntent()
+        var whatQuote = intent.getExtras()?.getString("Quotess");
+        when(p0!!.id){
+            (R.id.randomizer)->{
+                if(whatQuote == "Love"){
+
+                }
+            }
+        }
     }
 }
