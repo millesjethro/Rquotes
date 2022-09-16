cddab8ca6ec5a72248dd0f6bd5fc2cb8b76d4a69
Milles Jethro D Del Rosario <milles.jethro16@gmail.com>
2022-09-16 18:29:10 +0800

    Initial Commit

---
+++ app/src/main/java/com/auf/quotesrandomizer/QuoteRandomActivity.kt
@@ -0,0 +1,11 @@
+package com.auf.quotesrandomizer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuoteRandomActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
